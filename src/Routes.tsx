d34c3f5546efcc48a963d9a47069183362713cc1
Henrique Lopes <henrique.nobrega@ccc.ufcg.edu.br>
2020-06-30 22:57:16 -0300

    Add ProjectList page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import {BrowserRouter, Route} from "react-router-dom";
 import CreateProject from "./pages/CreateProject";
 import CreateUser from "./pages/CreateUser";
+import ProjectList from "./pages/ProjectList";
+import ProjectDetails from "./pages/ProjectDetails";
 
 const Routes: React.FC = () => {
   return (
     <BrowserRouter>
       <Route exact path="/create-project" component={CreateProject} />
       <Route exact path="/create-user" component={CreateUser} />
+      <Route exact path="/projects" component={ProjectList} />
+      <Route exact path="/projects/:id" component={ProjectDetails} />
     </BrowserRouter>
   );
 }
---
+++ src/pages/ProjectList/index.tsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react'
+import api from "../../services/api";
+import {Link} from "react-router-dom";
+import './styles.css'
+
+type Project = {
+  id: number;
+  name: string;
+}
+
+type Props = {
+  project: Project
+}
+
+const ProjectListItem: React.FC<Props> = (props) => {
+  const {project} = props;
+  return (
+    <div className="project-list-card">
+
+      <span className="project-list-title">{project.name}</span>
+      <Link to={`/projects/${project.id}`} className="project-list-link"> Visualizar projeto e seus membros</Link>
+    </div>
+  )
+}
+
+const ProjectList: React.FC = () => {
+
+  const [projects, setProjects] = useState<Project[]>([]);
+
+  useEffect(() => {
+    api.get<Project[]>("/projects").then((response) => {
+      setProjects(response.data);
+    })
+  }, [])
+
+  return (
+    <div>
+      <h2 className="title">Lista de projetos</h2>
+
+      <div className="project-grid">
+        {
+          projects.map((project) => (
+            <ProjectListItem project={project}/>
+          ))
+      }
+      </div>
+
+    </div>
+  );
+}
+
+export default ProjectList;
